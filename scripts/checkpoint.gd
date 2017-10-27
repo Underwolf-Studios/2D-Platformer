c93931940df8105e4fd5eef085eddc7257fe6da9
Adrian Zech <zech.adrian.at@gmail.com>
2017-10-27 22:12:08 +0200

    Created Basic Checkpoint and Respawn System

---
+++ scripts/checkpoint.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+onready var spr_active = load("res://sprites/circle-orange.png")
+
+var checkpoint_active = false
+
+func _on_checkpoint_body_entered(body):
+	if body.is_in_group("player") and !checkpoint_active:
+		checkpoint_active = true
+		$sprite.set_texture(spr_active)
+
+		body.respawn_position = position
+
+		print("DEBUG: Checkpoint activated")
