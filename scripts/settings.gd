05145285f0af776c8a7fe2103854e5b7e0bb1902
Adrian Zech <zech.adrian.at@gmail.com>
2017-11-01 10:47:45 +0100

    Improved Settings System

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -5,11 +5,10 @@ signal settings_loaded
 const SAVE_PATH = "res://config.cfg"
 var config_file = ConfigFile.new()
 
-# Display
 var fullscreen = false
 
 func _ready():
-	connect("settings_loaded", self, "_apply_settings")
+	connect("settings_loaded", self, "_on_settings_loaded")
 
 func load_settings():
 	var error = config_file.load(SAVE_PATH)
@@ -21,7 +20,7 @@ func load_settings():
 
 		config_file.save(SAVE_PATH)
 	else:
-		load_from_cfg(config_file, "display", "fullscreen")
+		load_from_config("display", "fullscreen")
 
 	emit_signal("settings_loaded")
 	print("INFO: Settings Loaded")
@@ -34,17 +33,17 @@ func save_to_config(section, key, value):
 		config_file.set_value(section, key, value)
 		config_file.save(SAVE_PATH)
 
-func load_from_cfg(config, section, key):
+func load_from_config(section, key):
 	if config_file.has_section_key(section, key):
 		set(key, config_file.get_value(section, key))
 	else:
 		print("WARNING: '" + key + "' missing from '" + section + "' section in the config file, default value has been added.")
 		save_to_config(section, key, get(key))
 
-func _apply_settings():
-	#Display
+func _on_settings_loaded():
+	# Display
 	OS.set_window_fullscreen(fullscreen)
-#	$"settings_menu/tab_container/DISPLAY/fullscreen".set_pressed(fullscreen)
+	$"/root/main_menu/settings_menu/tab_container/DISPLAY/fullscreen".set_pressed(fullscreen)
 
 func _on_fullscreen_toggled(pressed):
 	save_to_config("display", "fullscreen", pressed)
